8166e3d710a0dca5bb33350924e59017400bb645
Thanos002 <uyiky@student.kit.edu>
2023-05-22 20:40:51 +0200

    cambio Iago Elevadores

--- PruebaGlobal/PruebaGlobal/Elevadores.c
+++ PruebaGlobal/PruebaGlobal/Elevadores.c
@@ -12,19 +12,15 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include <avr/sfr_defs.h>
 
 #include "IncFile1.h"
-#include "Elevadores.h"
-
-volatile uint8_t ERpos;
-volatile uint8_t ECPos;
-volatile uint8_t rutina=0;
-volatile uint8_t current_time=0;
 
 
-// para debugging
-// #define _delay_ms(P) rutina=rutina;
+volatile int ERpos;
+volatile int ECPos;
+volatile int rutina=0;
+volatile int Time=0;
+
 
 
 //MOTORES
@@ -37,10 +33,10 @@ volatile uint8_t current_time=0;
 //PULSADOR DIRSPARO
 // SW6: PD2 INT2
 
-// rutina 0: cuando llega al fin se para
-// rutina 1: cuando llega arriba baja
 
 //SET UP//
+
+
 
 void setupEC(){
 	setBit(M1_diDDR,M1_di_X);			//M1_diDDR=0x01
@@ -52,8 +48,8 @@ void setupEC(){
 void setupER(){
 	setBit(M5_diDDR,M5_di_X);			//M5_diDDR=0x01;
 	setBit(M5_enDDR,M5_en_X);			//M5_enDDR=0x01;
-	EICRA|=0x80;						//flanco de bajada
-	EIMSK|=0x08;						//INT3
+ 	EICRA|=0x80;						//flanco de bajada
+ 	EIMSK|=0x08;						//INT3
 	clearBit(M5_enPORT,M5_en_X);		//M5_enPORT=0;
 }
 
@@ -66,39 +62,28 @@ void setupElevadores(){
 
 
 
-//Rutina de espera
-// REEMPLAZADA POR _DELAY_MS PARA MAYOR EFICIENCIA
-/*
-void wait(int t){
-	volatile int t0=current_time;
-	while( current_time-t0<t){}
-}
-*/  
-
 //FUNCIONES EC//
 
 
-void bajaEC(){
+inline void bajaEC(){
 	clearBit(M1_enPORT,M1_en_X);			// antes de cambiar dir es necesario deshabilitar el motor
 	if(ECPos==1){
 		clearBit(M1_diPORT,M1_di_X);		//M1_diPORT=0x00;
 		setBit(M1_enPORT,M1_en_X);			//M1_enPORT=0x01;
 		_delay_ms(1000);
-		loop_until_bit_is_clear(SW1PIN,SW1X);
-		// while((PIND & 0x40)!=0){}			//Espera a que vuelva a saltar SW
+		while((PIND & 0x40)!=0){}			//Espera a que vuelva a saltar SW
 		clearBit(M1_enPORT,M1_en_X);		//M1_enPORT=0
 		ECPos=0;
 	}
 }
 
-void subeEC(){
+inline void subeEC(){
 	clearBit(M1_enPORT,M1_en_X);			// antes de cambiar dir es necesario deshabilitar el motor
 	if(ECPos==0){
 		setBit(M1_diPORT,M1_di_X);			//M1_diPORT=0;
 		setBit(M1_enPORT,M1_en_X);			//M1_enPORT=1;
 		_delay_ms(1000);					//Espera a que el SW1 se haya dejado de pulsar.
-		loop_until_bit_is_clear(SW1PIN,SW1X);
-		// while((PIND & 0x40)==1){}			//Espera a que vuelva a saltar SW
+		while((PIND & 0x40)!=0){}			//Espera a que vuelva a saltar SW
 		clearBit(M1_enPORT,M1_en_X);		//M1_enPORT=0
 		ECPos=1;
 	}
@@ -123,54 +108,74 @@ void homeER(){
 	_delay_ms(3000);					//Espera 3 segundos para asegurarse de que sube
 	clearBit(M5_enPORT,M5_en_X);
 	ERpos=0;
+
 }
 
 //FUNCIONES ER//
 
 
-void bajaER(){
+inline void bajaER(){
 	if(ERpos==1){
+				
+setBit(EIFR,3);
+
 		clearBit(M5_enPORT,M5_en_X);
 		clearBit(M5_diPORT,M5_di_X);	//M5_diPORT=0x01;
-		setBit(M5_enPORT,M5_en_X);		//M5_enPORT=0x01;
+		setBit(M5_enPORT,M5_en_X);
+		
+	
+			
 	}
 }
 
 
-void subeER(){
+inline void subeER(){
 	if(ERpos==0){
+				
+	setBit(EIFR,3);
+	
 		clearBit(M5_enPORT,M5_en_X);
 		setBit(M5_diPORT,M5_di_X);	//M5_diPORT=0x00;
-		setBit(M5_enPORT,M5_en_X);	//M5_enPORT=0x01;
+		setBit(M5_enPORT,M5_en_X);
+		
+			
+
 	}
 }
-//Devuelve la posición de ER
-
-int getEstadoER(){
-	return ERpos;
-}
-
-
 
 
 //INTERRUPCIONES//
 
 
 inline void OnSW5Interruption(){
+		setBit(EIFR,3);
+		clearBit(EIMSK,3);
+		Time=0;
 	if(rutina==0){
-		if(readBit(M5_diPORT,M5_di_X)==0){	
+		if(readBit(M5_diPIN,M5_di_X)==0){	
 			ERpos=0;
 			clearBit(M5_enPORT,M5_en_X);
 		}
-		if(readBit(M5_diPORT,M5_di_X)==1){	
+		if(readBit(M5_diPIN,M5_di_X)==1){	
 			ERpos=1;
 			clearBit(M5_enPORT,M5_en_X);
 		}
 	}
-	if (rutina==1){			//Rutina de reacomodo
-		ERpos = 1;
+	else if (rutina==1){			//Rutina de reacomodo
+		ERpos=1;
 		rutina=0;
 		bajaER();
+		
+		}
+			
+	
+	
+}
+inline void UpdateTimerElevadores(){
+	Time=Time+5;
+	if(Time>=500){
+		setBit(EIMSK,3);
+		setBit(EIFR,3);
 	}
 }
 
@@ -178,30 +183,13 @@ inline void OnSW5Interruption(){
 
 	//FUNCIONES COMPLEJAS//
 
-void recarga(){ 
+inline void recarga(){
+	
 	rutina=1;
 	subeER();
 }
 
 
 
-/*
-int main(void)
-{
-	setupElevadores();
-	
-	homeER();
-	//homeEC();
-	
-	subeER();
-	_delay_ms(2000);
-	bajaER();
 
-	
-     while (1) 
-	
-    {
-	
-    }
-}
-*/
+
--- PruebaGlobal/PruebaGlobal/Elevadores.h
+++ PruebaGlobal/PruebaGlobal/Elevadores.h
@@ -1,34 +1,11 @@
-#include <avr/io.h>
+#ifndef Elevadores_h__
+#define Elevadores_h__
 
-#ifndef ELEVADORES_H_
-#define	ELEVADORES_H_
+void setupEC(){};
+void setupER(){};
+void setupElevadores(){};
+void homeEC(){};
+void homeER(){};
 
-//Prototipo de funciones
 
-void setuptimer();
-
-void setupEC(); 
-
-void setupER();
-
-void bajaEC();
-
-void subeEC();
-
-void homeEC();
-int waitforEC(int estado);//La variable estado correspode a la posición en la que debe acabar el elevador . El programa esperará hasta que llegue a ella;
-
-void homeER();
-void bajaER();
-
-void subeER();
-
-int getEstadoER();//Devuelve la posición del ER
-
-void UpdateTimeElevadores();
-void setupPulsador();
-void setupElevadores();
-void OnSW5Interruption();
-
-void recarga();//Le pasamos el tiempo de delay y la condición del ultimo disparo y procede a recargar la bola
 #endif
