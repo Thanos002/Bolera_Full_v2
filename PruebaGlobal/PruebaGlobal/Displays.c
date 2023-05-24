e35bf81dd80c794e460a91b2d4b028e60d8099a1
Thanos002 <uyiky@student.kit.edu>
2023-05-24 14:09:51 +0200

    Macros Displays
    Rollback si no funciona

--- PruebaGlobal/PruebaGlobal/Displays.c
+++ PruebaGlobal/PruebaGlobal/Displays.c
@@ -11,8 +11,6 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#define F_CPU 8000000UL
-#include <util/delay.h>
 
 volatile uint16_t parpadeos=0;  // volatile pues se cambia con INT
 volatile uint8_t finalizadoFlag = 0;
@@ -37,7 +35,8 @@ volatile uint8_t puntuacion;
 */
 
 inline void apagarDisplay(){
-	PORTB &= ~(1<<PB6)&~(1<<PB5)&~(1<<PB4)&~(1<<PB3)&~(1<<PB2)&~(1<<PB1)&~(1<<PB0);  //Todos los bits de esos puertos a 1_
+	clearBitMask(DSPORT, 0b01111111);
+	//PORTB &= ~(1<<PB6)&~(1<<PB5)&~(1<<PB4)&~(1<<PB3)&~(1<<PB2)&~(1<<PB1)&~(1<<PB0);  //Todos los bits de esos puertos a 0
 }
 
 inline void setDisplay(int numero){
@@ -47,51 +46,51 @@ inline void setDisplay(int numero){
 	switch(numero)
 	{
 		case 0:	//0b00111111;
-		PORTB |= (1<<PB0)|(1<<PB1)|(1<<PB2)|(1<<PB3)|(1<<PB4)|(1<<PB5);
+		DSPORT |= (1<<DaX)|(1<<DbX)|(1<<DcX)|(1<<DdX)|(1<<DeX)|(1<<DfX);
 		break;
 		case 1:	//0b00000110;
-		PORTB |= (1<<PB1)|(1<<PB2);
+		DSPORT |= (1<<DbX)|(1<<DcX);
 		break;
 		case 2: //0b01011011;
-		PORTB |= (1<<PB0)|(1<<PB1)|(1<<PB3)|(1<<PB4)|(1<<PB6);
+		DSPORT |= (1<<DaX)|(1<<DbX)|(1<<DdX)|(1<<DeX)|(1<<DgX);
 		break;
 		case 3: //0b01001111;
-		PORTB |= (1<<PB6)|(1<<PB3)|(1<<PB2)|(1<<PB1)|(1<<PB0);
+		DSPORT |= (1<<DgX)|(1<<DdX)|(1<<DcX)|(1<<DbX)|(1<<DaX);
 		break;
 		case 4: //0b01100110;
-		PORTB |= (1<<PB1)|(1<<PB2)|(1<<PB5)|(1<<PB6);
+		DSPORT |= (1<<DbX)|(1<<DcX)|(1<<DfX)|(1<<DgX);
 		break;
 		case 5: //0b01101101;
-		PORTB |= (1<<PB0)|(1<<PB2)|(1<<PB3)|(1<<PB5)|(1<<PB6);
+		DSPORT |= (1<<DaX)|(1<<DcX)|(1<<DdX)|(1<<DfX)|(1<<DgX);
 		break;
 		case 6: //0b01111101;
-		PORTB |= (1<<PB0)|(1<<PB5)|(1<<PB2)|(1<<PB3)|(1<<PB4)|(1<<PB6);
+		DSPORT |= (1<<DaX)|(1<<DfX)|(1<<DcX)|(1<<DdX)|(1<<DeX)|(1<<DgX);
 		break;
 		case 7: //0b00000111;
-		PORTB |= (1<<PB0)|(1<<PB1)|(1<<PB2);
+		DSPORT |= (1<<DaX)|(1<<DbX)|(1<<DcX);
 		break;
 		case 8: //0b01111111;
-		PORTB |= (1<<PB0)|(1<<PB1)|(1<<PB2)|(1<<PB3)|(1<<PB4)|(1<<PB5)|(1<<PB6);
+		DSPORT |= (1<<DaX)|(1<<DbX)|(1<<DcX)|(1<<DdX)|(1<<DeX)|(1<<DfX)|(1<<DgX);
 		break;
 		case 9: //0b01101111;
-		PORTB |= (1<<PB0)|(1<<PB1)|(1<<PB2)|(1<<PB5)|(1<<PB6);
+		DSPORT |= (1<<DaX)|(1<<DbX)|(1<<DcX)|(1<<DfX)|(1<<DgX);
 		break;
 		default:
-		PORTB |= (1<<PB6);
+		DSPORT |= (1<<DgX);
 	}
 }
 
 // Cambio Thanos: poner else if z meter condicion parpadeo>200 en parpadeos >=180 (comprobar porfa)
 // Declarada funcion inline, que se llama dentro del main()
-inline void DisplayUpdater(){
+inline void DisplayUpdater(){		//Se llama cada 5ms con el timer0
 	if(finalizadoFlag == 1){
 		parpadeos++;
 		if(parpadeos <= 180){
-			//Preguntar Jorge pero creo que PORT7B no vale, tiene que ser PORTXi --> PXi para los puertos de E/S. No lo que pone en el datasheet, para este caso.
-			PORTB ^= (1<<PB7);					//Máscara en el bit de seleccion para alternarlo cada 5 ms, es decir un refresco de 200 veces/segundo
+			
+			toggleBit(DSPORT,DSX);				//Máscara en el bit de seleccion para alternarlo cada 5 ms, es decir un refresco de 200 veces/segundo
 			//Posible bandera 'Selector
 			
-			if(PINB & (1 << PB7)){				//Si está seleccionado Display 1, decenas
+			if(~PINB & (1 << DSX)){				//Si está seleccionado Display 1, decenas
 				setDisplay(puntuacion%10);
 			}
 			else{								//Si esta seleccionado Display 0, unidades
@@ -99,7 +98,7 @@ inline void DisplayUpdater(){
 			}
 		}
 		else if(parpadeos >= 180){
-			PORTB = 0x00;  // Apagados y DS da igual porque al estar apagados da igual a que display este apuntando
+			DSPORT = 0x00;  // Apagados y DS da igual porque al estar apagados da igual a que display este apuntando
 			if(parpadeos == 200){
 				parpadeos = 0; // Reiniciar el contador de parpadeos
 			}
@@ -107,10 +106,10 @@ inline void DisplayUpdater(){
 	}
 	else{
 		//Preguntar Jorge pero creo que PORT7B no vale, tiene que ser PORTXi --> PXi para los puertos de E/S. No lo que pone en el datasheet, para este caso.
-		PORTB ^= (1<<PB7);					//Máscara en el bit de seleccion para alternarlo cada 5 ms, es decir un refresco de 200 veces/segundo
+		toggleBit(DSPORT,DSX);					//Máscara en el bit de seleccion para alternarlo cada 5 ms, es decir un refresco de 200 veces/segundo
 		//Posible bandera 'Selector
 		
-		if(~PINB & (1 << PB7)){				//Si está seleccionado Display 1, decenas
+		if(~PINB & (1 << DSX)){				//Si está seleccionado Display 1, decenas
 			setDisplay(puntuacion/10);
 		}
 		else{								//Si esta seleccionado Display 0, unidades
